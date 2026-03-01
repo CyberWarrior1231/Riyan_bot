092666839a29f34ecdfa65e3b5b23f30fb590ca0
CyberWarrior1231 <thestaralwayad@gmail.com>
2026-03-01 11:43:47 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,42 @@
+# Don't Remove Credit Tg - @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+import os
+
+# Login feature, if you want then True , if you don't want then False
+LOGIN_SYSTEM = bool(os.environ.get('LOGIN_SYSTEM', True)) # True or False
+
+if LOGIN_SYSTEM == False:
+    # if login system is False then fill your tg account session below 
+    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+else:
+    STRING_SESSION = None
+
+# Bot token @Botfather
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+# Your API ID from my.telegram.org
+API_ID = int(os.environ.get("API_ID", "29490954"))
+
+# Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "dbd8f5af56b0f6e16327c20a84eece99")
+
+# Your Owner / Admin Id For Broadcast 
+ADMINS = int(os.environ.get("ADMINS", "8327651421"))
+
+# Your Channel Id In Which Bot Upload Downloaded Video/File/Message etc.
+# And Make Your Bot Admin In this channel with full rights.
+# if you don't want to upload in channel then leave it blank don't fill anything.
+CHANNEL_ID = os.environ.get("CHANNEL_ID", "-1003850154506")
+
+# Your Mongodb Database Url
+# Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://fibegi:8oV4fjNNVasSfcoY@cluster0.jp8thup.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
+
+# Increase time as much as possible to avoid floodwait, spamming and tg account ban issues.
+WAITING_TIME = int(os.environ.get("WAITING_TIME", "10")) # time in seconds
+
+# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
+ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
